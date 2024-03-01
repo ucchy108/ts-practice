08c62bd312061748a8230cdba2050c6cf9754fb0
ucchy84 <k208576@ie.u-ryukyu.ac.jp>
2024-03-02 08:57:33 +0900

    add: 5555 ports

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,17 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Task } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('todo')
 export class TodoController {
-  constructor(
-    private prisma: PrismaService
-  ){}
+  constructor(private prisma: PrismaService) {}
 
   @Get('list')
-  async getList() {
-    const result = await this.prisma.task.findMany()
-    return [
-      ...result
-    ]
+  async getList(): Promise<Task[]> {
+    const result = await this.prisma.task.findMany();
+    return [...result];
   }
 }
