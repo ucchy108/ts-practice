a162120a14db908565243b4d77a6d19ea4900df9
ucchy108 <ucchy108@gmail.com>
2024-05-11 16:36:21 +0900

    feat: HabitのResolverを実装

--- api/src/habit/habit.module.ts
+++ api/src/habit/habit.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HabitService } from './habit.service';
+import { HabitResolver } from './habit.resolver';
 import { HabitController } from './habit.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { TasksModule } from '../tasks/tasks.module';
 
 @Module({
   imports: [PrismaModule, TasksModule],
-  providers: [HabitService],
+  providers: [HabitService, HabitResolver],
   controllers: [HabitController],
 })
 export class HabitModule {}
